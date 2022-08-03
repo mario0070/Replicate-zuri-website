747edf8041f8c98d0962343cea60d56710a831c6
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2022-08-03 17:51:25 +0100

    remote

---
+++ new.js
@@ -0,0 +1,7 @@
+// var tb1=document.getElementById("tb1")
+// var sp1=document.getElementById("span")
+// tb1.addEventListener("click",()=>{
+//     console.log("clicked")
+//     sp1.classList.toggle("valid")
+
+// })
